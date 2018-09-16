d4e15e8f7e3a592bad24ae10ec5ef03780309368
Sebastian <votex999@gmail.com>
2018-09-16 19:23:42 +0200

    stworzenie komponentu model

--- src/Components/Models/MainModel.jsx
+++ src/Components/Models/MainModel.jsx
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import './MainModel.css'
 
 class MainModel extends Component {
 
     render() {
         return (
-            <div className='containerConfiguration'>
+            <div className="modelContainer">
+                <div className='containerConfiguration'>
+                    <div className="configuration">
+                        <span>Skonfiguruj własny model</span>
+                        <div className="finishStyle">sportowy</div>
+                        <div className="engine">140koni</div>
+                        <div className="color">niebieski</div>
+                    </div>
+                    <div className="givenModel">
+                        <div className="imgModel">tutaj obrazek</div>
+                        <div className="sumEquipment">a tutaj informacja o kwocie</div>
+                    </div>
 
+                </div>
             </div>
         );
     }
