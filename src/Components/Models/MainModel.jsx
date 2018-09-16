2eba7cae79e669fd9fa5b598cfcc2efe62d001a9
Sebastian <votex999@gmail.com>
2018-09-16 20:17:29 +0200

    praca nad konfiguracja modelu

--- src/Components/Models/MainModel.jsx
+++ src/Components/Models/MainModel.jsx
@@ -2,16 +2,68 @@ import React, { Component } from 'react';
 import './MainModel.css'
 
 class MainModel extends Component {
+    constructor(props){
+        super(props);
+
+
+        const styleOptions = ['standard', 'sport', 's-line'];
+        const colorOptions = ['black/white' , 'orange/silver'];
+
+        const engineOptions = [
+            {
+                item: '140km 1600',
+                price: '4800',
+            },
+            {
+                item: '170km 1900',
+                price: '5500',
+            },
+            {
+                item: '235km 2200',
+                price: '7200',
+            },
+        ];
+
+        this.state = {
+            engineOptions,
+            styleOptions,
+            colorOptions,
+        }
+    }
+
+    handleChecked = (i) => {
+      console.log(i)
+    };
+
+    makingOptions = (array) => {
+        const givenArray = array;
+        return givenArray.map((e,i) => {
+            return <li className='givenOption' key={i}>
+                <div className="checkBox" onClick={() => this.handleChecked(i)} />
+                <span className='item' >{e.item + ' cm3'}</span>
+                <span className='price' >{e.price}</span>
+            </li>
+        })
+
+    };
 
     render() {
         return (
             <div className="modelContainer">
                 <div className='containerConfiguration'>
                     <div className="configuration">
-                        <span>Skonfiguruj własny model</span>
-                        <div className="finishStyle">sportowy</div>
-                        <div className="engine">140koni</div>
-                        <div className="color">niebieski</div>
+                        <span className='configurationSpan'>Skonfiguruj własny model</span>
+                        <div className="finishStyle">
+
+                        </div>
+                        <div className="engine">
+                            <span>Moc/Pojemość Silnika</span>
+                            <ul className='engineList'>
+                                {this.makingOptions(this.state.engineOptions)}
+                            </ul>
+                        </div>
+                        <div className="color">
+                        </div>
                     </div>
                     <div className="givenModel">
                         <div className="imgModel">tutaj obrazek</div>
